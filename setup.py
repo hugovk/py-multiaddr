507f03207cf8a6352e425d0e0d05b4eca427df95
Hugo <hugovk@users.noreply.github.com>
2019-01-08 17:37:21 +0200

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     setup_requires=[
         'pytest-runner',
     ],
